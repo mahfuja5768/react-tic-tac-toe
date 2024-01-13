cc4960d53a4c7d05b63fc6dbad5a80c09aced2fd
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2024-01-13 17:19:21 +0600

    start winner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-function Square({value, handleSquareClick}) {
-
+function Square({ value, handleSquareClick }) {
   return (
     <button
       onClick={handleSquareClick}
@@ -12,29 +11,56 @@ function Square({value, handleSquareClick}) {
   );
 }
 
-export default function Board({handleSquareClick}) {
-  const [square, setSquares]= useState(Array(9).fill(null))
+export default function Board({ handleSquareClick }) {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
-
-  
+  function handleCLick(i) {
+    // console.log('c');
+    if (squares[i]) {
+      return;
+    }
+    const nextSquare = squares.slice();
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+    // setSquares([...squares])
+    setSquares(nextSquare);
+    setXIsNext(!xIsNext);
+  }
 
   return (
     <>
       <div className="flex">
-        <Square value={square[0]} handleSquareClick={handleCLick}/>
-        <Square value={square[1]}/>
-        <Square value={square[2]}/>
+        <Square value={squares[0]} handleSquareClick={() => handleCLick(0)} />
+        <Square value={squares[1]} handleSquareClick={() => handleCLick(1)} />
+        <Square value={squares[2]} handleSquareClick={() => handleCLick(2)} />
       </div>
       <div className="flex">
-        <Square value={square[3]}/>
-        <Square value={square[4]}/>
-        <Square value={square[5]}/>
+        <Square value={squares[3]} handleSquareClick={() => handleCLick(3)} />
+        <Square value={squares[4]} handleSquareClick={() => handleCLick(4)} />
+        <Square value={squares[5]} handleSquareClick={() => handleCLick(5)} />
       </div>
       <div className="flex">
-        <Square value={square[6]}/>
-        <Square value={square[7]}/>
-        <Square value={square[8]}/>
+        <Square value={squares[6]} handleSquareClick={() => handleCLick(6)} />
+        <Square value={squares[7]} handleSquareClick={() => handleCLick(7)} />
+        <Square value={squares[8]} handleSquareClick={() => handleCLick(8)} />
       </div>
     </>
   );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
 }
